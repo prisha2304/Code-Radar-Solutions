c0a9e7cde54e2220eecb7ada5a849b47272c297d
prisha2304 <prisha230906@gmail.com>
2025-03-20 21:36:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,16 +1,42 @@
-#include<stdio.h>
-int fibonacciSeries(int n){
-    if (n==0 ){
-        return 0;
-    }
-    else if(n==1){
-        return 1;
-    }
-    else{
-        return(fibonacciSeries(n-1)+fibonacciSeries(n-2));
-    }
-    for(int i=0;i<n;i++){
-        printf("%d ",fibonacciSeries(i));
-    }
+// #include<stdio.h>
+// int fibonacciSeries(int n){
+//     if (n==0 ){
+//         return 0;
+//     }
+//     else if(n==1){
+//         return 1;
+//     }
+//     else{
+//         return(fibonacciSeries(n-1)+fibonacciSeries(n-2));
+//     }
+//     for(int i=0;i<n;i++){
+//         printf("%d ",fibonacciSeries(i));
+//     }
+// }
+
+#include <stdio.h>
+
+// Function to compute nth Fibonacci number recursively
+int fibonacci(int n) {
+    if (n == 0) return 0; // Base case
+    if (n == 1) return 1; // Base case
+    return fibonacci(n - 1) + fibonacci(n - 2); // Recursive call
 }
 
+int main() {
+    int n;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    // Loop to print Fibonacci series
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    
+    return 0;
+}
