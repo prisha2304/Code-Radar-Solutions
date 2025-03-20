a69386c81909dc682c866960135c488ce93ae177
prisha2304 <prisha230906@gmail.com>
2025-03-20 21:22:32 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
-int fibonacciSeries(int n){
-    if (n==0 ){
-        printf("0");
-    }
-    else if(n==1){
-        printf ("1");
-    }
-    else{
-        printf("%d ", fibonacciSeries(n-1)+fibonacciSeries(n-2));
-    }
+// int fibonacciSeries(int n){
+//     if (n==0 ){
+//         return 0;
+//     }
+//     else if(n==1){
+//         return 1;
+//     }
+//     else{
+//         printf("%d ", fibonacciSeries(n-1)+fibonacciSeries(n-2));
+//     }
+// }
+int fibonacciSeries(int n) {
+    if (n == 0) return 0; // Base case 1
+    if (n == 1) return 1; // Base case 2
+    return fibonacciSeries(n - 1) + fibonacciSeries(n - 2); // Recursive call
+}
+
+// Function to print Fibonacci series using recursion
+void printFibonacciSeries(int n, int i) {
+    if (i == n) return; // Base condition to stop recursion
+    printf("%d ", fibonacciSeries(i)); // Print the ith Fibonacci number
+    printFibonacciSeries(n, i + 1); // Recursive call for next number
 }
