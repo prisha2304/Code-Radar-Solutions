29b8c6c397a9f9c5ef4eacd63482689606d2a41e
prisha2304 <prisha230906@gmail.com>
2025-04-07 20:54:38 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void factorial(int n) {
+int factorial(int n) {
     if (n == 0 || n == 1)
         return 1;
     return n * factorial(n - 1);
 }
-int factorialRange(int start, int end) {
+void factorialRange(int start, int end) {
     for (int i = start; i <= end; i++) {
         printf("%d\n", factorial(i));
     }
