9802863f1b078acc5bbba2ef5453280479fcc1d1
prisha2304 <prisha230906@gmail.com>
2025-04-09 01:29:35 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,19 +1,40 @@
 #include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int k;
-    scanf("%d",&k);
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]+arr[j]==k){
-                printf("%d %d\n",arr[i],arr[j]);
+void sort(int arr[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
+        }
     }
 }
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int k;
+    scanf("%d", &k);
+    // Sort the array to handle duplicates easily
+    sort(arr, n);
+
+    for(int i = 0; i < n; i++) {
+        // Skip duplicates
+        if(i > 0 && arr[i] == arr[i-1]) continue;
+
+        for(int j = i + 1; j < n; j++) {
+            // Skip duplicates
+            if(j > i + 1 && arr[j] == arr[j-1]) continue;
+
+            if(arr[i] + arr[j] == k) {
+                printf("%d %d\n", arr[i], arr[j]);
+            }
+        }
+    }
+
     return 0;
 }
